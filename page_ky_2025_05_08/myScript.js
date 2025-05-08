626fe4c8ebdf99518ce27110cd6e10ca08791e90
fpl-inf <rveklych.fplyceum@gmail.com>
2025-05-08 11:11:04 +0300

    Add K Y.

---
+++ page_ky_2025_05_08/myScript.js
@@ -0,0 +1,18 @@
+function generateTable() {
+	const tbl = document.createElement("table");
+	const tblBody = document.createElement("tbody");
+	for (let i = 0; i < 2; i++) {
+			const row = document.createElement("tr");
+			for (let j = 0; j < 2; j++) {
+					const cell = document.createElement ("td");
+					const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+					cell.appendChild(cellText);
+					row.appendChild(cell);
+			}
+			tblBody.appendChild(row);
+	}
+	tbl.appendChild(tblBody);
+	document.body.appendChild(tbl);
+	tbl.setAttribute("border", "2");
+}
+	
